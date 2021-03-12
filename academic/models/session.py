64b6006b79cf4676ad5993339f00038000a24166
alfahan <ali.farhan160@gmail.com>
2021-03-12 13:44:24 +0700

    Add Class Session

--- academic/models/session.py
+++ academic/models/session.py
@@ -5,3 +5,17 @@ class Course(models.Model):
 
     name = fields.Char("Nama", required=True, size=100)
     
+    course_id = fields.Many2one(comodel_name='academic.course', string='Course')
+    
+    instructor_id = fields.Many2one(comodel_name='res.partner', string='Instructor')
+    
+    start_date = fields.Date(string='Start Date')
+    
+    duration = fields.Integer(string='Duration')
+    
+    seats = fields.Integer(string='Seats')
+    
+    active = fields.Boolean("Is Active", default=True)
+    
+    
+    
