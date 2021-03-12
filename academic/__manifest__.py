b9e2c25adaf26f4acec5265f26857df486b2929e
alfahan <ali.farhan160@gmail.com>
2021-03-12 12:01:34 +0700

    Add Class

---
+++ academic/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name":"Academic",
+    "version": "1.0",
+    "depends": [
+        "base"
+    ],
+    "author" : "Mohamad Ali Farhan",
+    "category" : "Education",
+    "website" : "https://github.com/Alfahan",
+    "description" : "Ini adalah addon Akademik",
+    "data" : [
+        "views/menu.xml",
+    ],
+}
